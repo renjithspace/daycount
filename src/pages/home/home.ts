0f31602375a0535c5c50f20b64884f4a3c9cb41a
renjithspace <renjithrajeevvk@gmail.com>
2018-07-16 20:48:25 +0530

    Implement counter

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import  moment from 'moment'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	start = ""
+	end = ""
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
+  count() {
+  	if (!this.result && !this.end) return
+  	this.result = moment(this.end).diff(this.start, 'days')
   }
-
 }
